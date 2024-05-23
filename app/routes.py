acd600db1d9d2c41efc08d150ab1bd2c590140a9
GirmadaShaw <singhparam.7171@gmail.com>
2024-05-23 22:33:43 +0530

    routes.py minor issue solved

--- app/routes.py
+++ app/routes.py
@@ -470,7 +470,7 @@ def blob() :
 
     # start the webcam feed
     cap = cv2.VideoCapture(0)
-
+    maxindex = 0 
     while True:
         # Find haar cascade to draw bounding box around face
         ret, frame = cap.read()
